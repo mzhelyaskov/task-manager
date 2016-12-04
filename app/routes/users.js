6b797f65dfbfee0bebc605ffe43eeb00b1ba1ebf
Mykhailo Zheliaskov <mykhailo.zheliaskov@decerto.pl>
2016-12-04 13:57:49 +0100

    Project structure refactoring

--- app/routes/users.js
+++ app/routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource users');
 });
 
+router.get('/:id', function(req, res, next) {
+  res.send('respond with a resource users');
+});
+
 module.exports = router;
